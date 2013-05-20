487527d02ece6f0f3ee59b2f6a88ace835b3f221
Leo Dion <leogdion@brightdigit.com>
2013-05-20 16:41:55 -0400

    starting project

---
+++ ios/bindy/helper.h
@@ -0,0 +1,15 @@
+//
+//  helper.h
+//  bindy
+//
+//  Created by Leo G Dion on 5/20/13.
+//  Copyright (c) 2013 Leo Dion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface helper : NSObject
+
+- (NSString*) helloWorld;
+
+@end
---
+++ ios/bindy/helper.m
@@ -0,0 +1,17 @@
+//
+//  helper.m
+//  bindy
+//
+//  Created by Leo G Dion on 5/20/13.
+//  Copyright (c) 2013 Leo Dion. All rights reserved.
+//
+
+#import "helper.h"
+
+@implementation helper
+
+- (NSString*) helloWorld {
+    return @"Hello World";
+}
+
+@end
